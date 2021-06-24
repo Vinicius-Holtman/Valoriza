c340a4472f6df6e7c3d33036b38afbb55ecef8b5
Vinicius Holtman <42218598+Vinicius-Holtman@users.noreply.github.com>
2021-06-24 00:54:24 -0300

    Valoriza v4.0.0

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,48 @@
+import { getCustomRepository } from "typeorm"
+import { UsersRepositories } from "../repositories/UsersRepositories"
+import { compare } from "bcryptjs"
+import { sign } from "jsonwebtoken"
+
+
+
+interface IAuthenticateRequest {
+    email: string;
+    password: string;
+}
+
+class AuthenticateUserService {
+    async execute({ email, password }: IAuthenticateRequest) {
+        const usersRepositories = getCustomRepository(UsersRepositories);
+
+        // Verificar se email existe
+        const user = await usersRepositories.findOne({
+            email
+        });
+
+        if (!user) {
+            throw new Error("Email/Password incorrect");
+        }
+
+        //verificar se senha esta correta
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new Error("Email/Password incorrect");
+        }
+
+        //gerar token
+        const token = sign(
+            {
+                email: user.email
+            },
+            "3b741ccec5f827424cfef2e8fed0cfd7", 
+            {
+                subject: user.id,
+                expiresIn: "1d"
+            }
+        );
+
+        return token;
+    };
+}
+export { AuthenticateUserService }
