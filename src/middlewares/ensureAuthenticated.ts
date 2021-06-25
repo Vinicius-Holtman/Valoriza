9e48143daa82fd7beb0412ee829468521598f1b3
Vinicius Holtman <42218598+Vinicius-Holtman@users.noreply.github.com>
2021-06-25 11:19:54 -0300

    Valoriza 5.1.0

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,11 +1,30 @@
 import { Request, Response ,NextFunction} from "express";
+import { verify } from "jsonwebtoken"
+
+interface IPayLoad {
+    sub: string;
+}
 
 export function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
     // receber o token
+        const authToken = req.headers.authorization
+
+        //validar se token esta preenchido
+        if (!authToken) {
+            return res.status(401).end();
+        }
+
+        const [, token] = authToken.split(" ");
+
+        try {
+            //validar se token e valido
+            const { sub } = verify( token, "3b741ccec5f827424cfef2e8fed0cfd7") as IPayLoad;
+            req.user_id = sub;
+
+            return next();
+        } catch (err) {
+            return res.status(401).end();
+        }
         
-    //validar se token esta preenchido
-
-    //validar se token e valido
-
-    //recuperar informacoes usuario
+        //recuperar informacoes usuario
 }
