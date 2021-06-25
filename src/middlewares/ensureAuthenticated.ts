91293e1ae9c05ffbf5ee50ef5b9f4205eb467d12
Vinicius Holtman <42218598+Vinicius-Holtman@users.noreply.github.com>
2021-06-25 00:24:17 -0300

    Create ensureAutheticated.ts

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,11 @@
+import { Request, Response ,NextFunction} from "express";
+
+export function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
+    // receber o token
+        
+    //validar se token esta preenchido
+
+    //validar se token e valido
+
+    //recuperar informacoes usuario
+}
